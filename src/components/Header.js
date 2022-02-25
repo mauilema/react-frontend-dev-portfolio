bdf6924ec32e8d291137a34e7ba24f5c133facdc
mauilema <srjorgemaciel2002@gmail.com>
2022-02-24 23:53:37 -0500

    updated nav height

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ class Header extends Component {
     }, (props, prevProp) => true);
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 425, display: 'block' }}>
+      <header id="home" style={{ height: window.innerHeight - 450, display: 'block' }}>
         <div className="row aligner" style={{height: '100%'}}>
           <div className="col-md-12 div-wrapper">
             <div>
