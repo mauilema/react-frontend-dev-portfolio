569563a02dc445d8f2528481ab3ffe90df63604f
mauilema <srjorgemaciel2002@gmail.com>
2022-03-01 23:18:21 -0500

    changed the font family for the nav text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -29,7 +29,7 @@ class Nav extends Component {
             </a>
           </div>
   
-          <div id="navbarBasicExample" class="navbar-menu is-size-3">
+          <div id="navbarBasicExample" class="navbar-menu is-size-3 is-family-sans-serif">
             <div class="navbar-start">
               <a class="navbar-item">
                 Home
